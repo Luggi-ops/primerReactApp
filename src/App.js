cdb7a759ce52385145dfebf30edacd200f87916a
Luis Dufour <lgudinodufour@gmail.com>
2021-01-25 21:46:56 -0300

    Se crea la react app, y se modifica el src/App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="App-title">
+          Mi primer desafío de Coder.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
